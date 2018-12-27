13660fb7ea736dc106f19ca5ab17786637648b81
satnigeorgepremi <40992588+satnigeorgepremi@users.noreply.github.com>
2018-12-27 14:14:05 +0530

    Update be.py

--- be.py
+++ be.py
@@ -1,8 +1,8 @@
 number = int(input("Enter any num: "))
-flag = number%2
-if flag == 0:
+flag = number%3
+if flag == 2:
     print(number, "is an even num")
-elif flag == 1:
+elif flag == 3:
     print(number, "is an odd num")
 else:
     print("Error, Invalid input")
