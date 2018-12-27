83ff8aed6e65f5f01c26dd8eed27245bdf6a3e65
satnigeorgepremi <40992588+satnigeorgepremi@users.noreply.github.com>
2018-12-27 13:59:11 +0530

    Update C.py

--- C.py
+++ C.py
@@ -1,7 +1,7 @@
 ch = input("Enter a character: ")
 
-if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
- or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+if(ch=='B' or ch=='b' or ch=='C' or ch =='c' or ch=='D'
+ or ch=='d' or ch=='E' or ch=='e' or ch=='F' or ch=='f'):
     print(ch, "is a Vowel")
 else:
     print(ch, "is a Consonant")
