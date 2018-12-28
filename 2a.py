11ff9b733d41df4d0eb5933fbf2a0f9fafea0eb5
satnigeorgepremi <40992588+satnigeorgepremi@users.noreply.github.com>
2018-12-28 14:58:30 +0530

    Update 2a.py

--- 2a.py
+++ 2a.py
@@ -1,9 +1,9 @@
 
 def powers(base,exp):
-    if(exp==1):
-        return(base)
-    if(exp!=1):
-        return(base*powers(base,exp-1))
+if(exp==1):
+return(base)
+if(exp!=1):
+return(base*powers(base,exp-1))
 base=int(input("Enter base: "))
 exp=int(input("Enter exponential value: "))
 print("Result:",powers(base,exp))
