9d807ab48e7f274c8eb88fecd25b5df5d4f4adae
satnigeorgepremi <40992588+satnigeorgepremi@users.noreply.github.com>
2018-12-28 13:55:09 +0530

    Update 2b.py

--- 2b.py
+++ 2b.py
@@ -1,10 +1,10 @@
-m=int(input("Enter number:"))
-temp=m
+a=int(input("Enter number:"))
+temp=a
 rev=0
 while(n>0):
-    dig=m%10
+    dig=a%10
     rev=rev*10+dig
-    m=n
+    a=n
 if(temp==rev):
     print("The number is a palindrome!")
 else:
