83ac78c7574d958282ca4419e2e20b97db7a1ba9
satnigeorgepremi <40992588+satnigeorgepremi@users.noreply.github.com>
2018-12-28 15:09:24 +0530

    Delete 2a.py

--- 2a.py
+++
@@ -1,9 +0,0 @@
-
-def powers(base,exp):
-if(exp==2):
-return(base)
-if(exp!=2):
-return(base*powers(base,exp-2))
-base=int(input("Enter base: "))
-exp=int(input("Enter exponential value: "))
-print("Result:",powers(base,exp))
