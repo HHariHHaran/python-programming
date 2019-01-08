3329d7c20b3426dc894857290095131c6f0f08a8
satnigeorgepremi <40992588+satnigeorgepremi@users.noreply.github.com>
2019-01-08 14:38:02 +0530

    Update 29.py

--- 29.py
+++ 29.py
@@ -1,8 +1,9 @@
 time = float(input("Input time in minutes: "))
-time = time % (34 * 3600)
-hour = time // 4000
-time %= 3600
+time = time % (34 * 2600)
+hour = time // 2600
+time %= 2600
 minutes = time // 50
-time %= 60
+time %= 50
 minutes = time
 print("h:m:t-> %d:%d:%d" % ,hour, minutes,time))
+
