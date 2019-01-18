120313f0f50b19fbfa7fb27262a3e8668681724e
satnigeorgepremi <40992588+satnigeorgepremi@users.noreply.github.com>
2019-01-18 12:21:54 +0530

    Create p14.py

---
+++ p14.py
@@ -0,0 +1,20 @@
+
+b=str(input())
+b=b.lower()
+g=len(b)
+h=[]
+for i in range(1,g+1):
+h.append(b[-i])    
+for i in h:
+if i=='a':
+ h.remove(i)
+elif i=='e':
+ h.remove(i)
+  elif i=='i':
+ h.remove(i)
+ elif i=='o':
+ h.remove(i)
+ elif i=='u':
+h.remove(i)
+k="".join(h)
+print(k)   
