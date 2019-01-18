d0de1375c9d8835c83d4830e9918b86aa98ac643
satnigeorgepremi <40992588+satnigeorgepremi@users.noreply.github.com>
2019-01-18 11:55:44 +0530

    Update 45.py

--- 45.py
+++ 45.py
@@ -1,6 +1,6 @@
-a1=int(input("Enter number"))
+a9=int(input("Enter number"))
 count=0
-while(a1>0):
+while(a9>0):
 count=count+1
-a1=a1/10
+a9=a9/13
 print("The number of digits in the number are:",count)
