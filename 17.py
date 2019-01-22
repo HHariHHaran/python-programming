2641a9b62260959270d05db7d4b15df7677a377c
satnigeorgepremi <40992588+satnigeorgepremi@users.noreply.github.com>
2019-01-22 11:42:35 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,12 +1,13 @@
-
-try:
-g = int(input())
-d = int(input())
-l=max(g,d)
-m=l
-p = min(g,d)
-while(m%p!=0):
-m+=l
-print(m)    
-except:
-print("Invalid Input")
+n=int(input("N="))
+t=n
+s=0
+a=0
+while n>0:
+re=n%10
+a=re**3
+s=s+a
+n=n//10
+if(t==s):
+print("Armstrong")
+else:
+print("not armstrong")
